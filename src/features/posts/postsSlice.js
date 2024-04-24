f1172e4aa429ce49f56c90da080db4b7790b676b
prashantsingh181 <prashant@poditservices.com>
2024-04-24 19:15:25 +0530

    adding async thunks

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,19 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from "axios";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+const initialState = {
+  error: null,
+  state: "idle",
+  posts: [],
+};
+
+const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -36,7 +21,7 @@ const postsSlice = createSlice({
   reducers: {
     addPost: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, author) {
         return {
@@ -60,7 +45,7 @@ const postsSlice = createSlice({
     addReaction: {
       reducer(state, action) {
         const { postId, reaction } = action.payload;
-        const existingPost = state.find((post) => post.id === postId);
+        const existingPost = state.posts.find((post) => post.id === postId);
         if (existingPost) {
           existingPost.reactions[reaction]++;
         }
@@ -79,4 +64,4 @@ const postsSlice = createSlice({
 
 export default postsSlice.reducer;
 export const { addPost, addReaction } = postsSlice.actions;
-export const postsSelector = (state) => state.posts;
+export const postsSelector = (state) => state.posts.posts;
